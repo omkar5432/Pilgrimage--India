74f51e8e1f9154c210091c78f727607462c0e5f8
Omkar Patilba Thube <othub@Omkar>
2024-05-25 10:54:12 +0530

    commit

--- src/main/java/com/example/demo/PilgriamgeApplication.java
+++ src/main/java/com/example/demo/PilgriamgeApplication.java
@@ -8,6 +8,7 @@ public class PilgriamgeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PilgriamgeApplication.class, args);
+		System.out.println("Application Started");
 	}
 
 }
